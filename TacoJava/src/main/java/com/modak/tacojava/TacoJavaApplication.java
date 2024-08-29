62b79233a3afab3fe44cca0dd346931ef466d2ba
itmdeveloper <choidasol95@gmail.com>
2024-08-29 19:30:43 +0900

    연습용 프로젝트 생성

---
+++ TacoJava/src/main/java/com/modak/tacojava/TacoJavaApplication.java
@@ -0,0 +1,13 @@
+package com.modak.tacojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoJavaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TacoJavaApplication.class, args);
+  }
+
+}
---
+++ TacoKt/src/main/kotlin/com/modak/tacokt/TacoKtApplication.kt
@@ -0,0 +1,11 @@
+package com.modak.tacokt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TacoKtApplication
+
+fun main(args: Array<String>) {
+    runApplication<TacoKtApplication>(*args)
+}
