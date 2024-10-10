adfd25f52ed3a4961dd43ac8b10b6fe8947e2912
itmdeveloper <choidasol95@gmail.com>
2024-10-10 23:43:22 +0900

    스프링 구성 속성 사용

--- TacoJava/src/main/java/com/exam/tacojava/controller/OrderController.java
+++ TacoJava/src/main/java/com/exam/tacojava/controller/OrderController.java
@@ -1,11 +1,14 @@
 package com.exam.tacojava.controller;
 
+import com.exam.tacojava.config.OrderProps;
 import com.exam.tacojava.domain.Order;
 import com.exam.tacojava.domain.User;
 import com.exam.tacojava.repository.OrderRepository;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,6 +26,11 @@ import org.springframework.web.bind.support.SessionStatus;
 @RequiredArgsConstructor
 public class OrderController {
 
+  /*
+  @Value("${taco.orders.pageSize}")
+  private final int pageSize = 20;
+  */
+  private final OrderProps orderProps;
   private final OrderRepository orderRepository;
 
   @GetMapping("/current")
@@ -47,4 +55,12 @@ public class OrderController {
     return "redirect:/";
   }
 
+  @GetMapping
+  public String ordersForUser(
+      @AuthenticationPrincipal User user,
+      Model model){
+    Pageable pageable = PageRequest.of(0, orderProps.getPageSize());
+    orderRepository.findByUserOrderByPlacedAtDesc(user, pageable);
+    return "orderList";
+  }
 }
--- TacoJava/src/main/java/com/exam/tacojava/repository/OrderRepository.java
+++ TacoJava/src/main/java/com/exam/tacojava/repository/OrderRepository.java
@@ -1,10 +1,14 @@
 package com.exam.tacojava.repository;
 
 import com.exam.tacojava.domain.Order;
+import com.exam.tacojava.domain.User;
+import java.util.List;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends CrudRepository<Order, Long> {
 
+  List<User> findByUserOrderByPlacedAtDesc(User user, Pageable pageable);
 }
